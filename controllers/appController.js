12bfb397110dbe826ae38ac4089fb9ebd4815661
Georg <gwbootcamp24.google.com>
2024-05-10 02:24:32 +0200

    changes canvas Size

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,6 +1,7 @@
 
 import {pool} from '../utils/db.js';
-import { createCanvas, loadImage, registerFont } from 'canvas';
+import { createCanvas, loadImage, registerFont, Image } from 'canvas';
+// import axios, {isCancel, AxiosError} from 'axios';
 import * as fs from 'fs';
 import * as http from 'http'; 
 export async function getComposedImage(req, res, next) {
@@ -11,7 +12,7 @@ export async function getComposedImage(req, res, next) {
 export async function getAllData(req, res, next) {
   try {
     const {rows: components} = await pool.query(
-      `SELECT id,type,url,url2, ROW_NUMBER() OVER (
+      `SELECT *, ROW_NUMBER() OVER (
       PARTITION BY type
       ORDER BY id DESC) 
       AS row_number 
@@ -72,36 +73,88 @@ export async function cleanupFiles(req, res, next) {console.log("req.identifier"
 
 
 export async function composeImage(req, res, next) {
-  // todo try catch und async Handler
+
   try{
-    const background = req.body.background? await loadImage(`./${req.body.background}`):'';
+    
+    let background
+    
+    console.log("req.body.background",req.body.background);
+
+    if (req.body.background?.match('blob')){
+      // background = new Image(); // Create a new Image
+      // background.src = req.body.background;
+      // const background = req.body.background? await loadImage(Buffer.from(req.body.background).toString("base64")) :'';
+
+    async function savePhotoFromAPI() {
+
+
+      const res = await axios.get( req.body.background,
+         { responseType: "arraybuffer" }  //<--- ADDED THIS
+      );
+    
+
+
+      const response = await fetch(req.body.background);
+      const arrayBuffer = await response.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+
+
+      // const fileType = await FileType.fromBuffer(buffer);
+
+      // if (fileType.ext) {
+      if (1) {
+          const outputFileName = `yourfilenamehere.png`
+          fs.createWriteStream(outputFileName).write(buffer);
+      } else {
+          console.log('File type could not be reliably determined! The binary data may be malformed! No file saved!')
+      }
+
+    }
+    
+    savePhotoFromAPI();
+
+      // (async function() {
+      //   const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+      //   console.log(1)
+      //   background =  await loadImage(req.body.background) ;
+      //   await sleep(1000)
+      //   console.log(2)
+      //   // background.src = req.body.background;
+      //   await sleep(1000)
+      //   console.log("background",background)
+      // })()
+
+
+    } else {
+      background = req.body.background? await loadImage(`./${req.body.background}`):'';
+    }
+  
+    // const background = req.body.background? await loadImage(`./${req.body.background}`):'';
     const hair = req.body.hair? await loadImage(`./${req.body.hair}`):'';
     const head = req.body.head? await loadImage(`./${req.body.head}`):'';
     const body = req.body.body? await loadImage(`./${req.body.body}`):'';
     const legs = req.body.legs? await loadImage(`./${req.body.legs}`):'';
     const hands = req.body.body? await loadImage(`./img/hands.png`):'';
     const text = req.body.text;
-  // const logo = await loadImage(`./${req.identifier}-logo.jpg`);
 
 
-  // const width = background.width;
-  // const height = background.height;
   registerFont("./shortbaby2.ttf", { family: "ShortBaby" });
-  // const canvas = createCanvas(width, 2 * height);
-  const canvasWidth = 480;
-  const canvasHeight = 1000;
+
+  const canvasWidth = 1920;
+  const canvasHeight = 1080;
   const canvas = createCanvas(canvasWidth, canvasHeight);
   const context = canvas.getContext("2d");
+  const canvasXPos = canvas.width/2 - 240;
+
 
   const logoPadding = 20;
-  
-  // context.drawImage(background, 0, 0, background.width, background.height);
-  if (background) context.drawImage(background, 0, 0, canvasWidth, canvasHeight);
-  if (legs) context.drawImage(legs, 0, 428, legs.width, legs.height);
-  if (body) context.drawImage(body, 0, 195, body.width, body.height);
-  if (hands) context.drawImage(hands, 0, 195, hair.width, hair.height);
-  if (head) context.drawImage(head, 0, 0, head.width, head.height);
-  if (hair) context.drawImage(hair, 0, 0, hair.width, hair.height);
+  console.log("BACKGROUND",background)
+  if (background) context.drawImage(background, Math.floor(0.5*(-1 * background.width + canvasWidth)), Math.floor(0.5*(-1 * background.height + canvasHeight)), background.width, background.height);
+  if (legs) context.drawImage(legs, canvasXPos, 428, legs.width, legs.height);
+  if (body) context.drawImage(body, canvasXPos, 195, body.width, body.height);
+  if (hands) context.drawImage(hands, canvasXPos, 195, hair.width, hair.height);
+  if (head) context.drawImage(head, canvasXPos, 0, head.width, head.height);
+  if (hair) context.drawImage(hair, canvasXPos, 0, hair.width, hair.height);
 
   // context.drawImage(logo, width - logo.width - logoPadding + 400, height - logo.height - logoPadding);
 
@@ -149,7 +202,6 @@ export async function composeImage(req, res, next) {
   
   }
 
-  // context.fillText(req.body.text??'give me a name', textPadding, 920);
 
   const buffer = canvas.toBuffer("image/png");
   req.compositeImageBuffer = buffer;
@@ -161,3 +213,44 @@ export async function composeImage(req, res, next) {
 }
 
 }
+
+
+
+
+
+
+
+// if (fn) {
+//   this.toBuffer((err, buf) => {
+//     if (err) return fn(err)
+//     fn(null, `data:${type};base64,${buf.toString('base64')}`)
+//   }, type, opts)
+// } else {
+//   return `data:${type};base64,${this.toBuffer(type, opts).toString('base64')}`
+// }
+
+
+
+  // var canvas = document.getElementById('canvas');
+  // var input = document.getElementById('input');
+  
+  
+  //   function picToBlob() {
+  //     canvas.renderImage(input.files[0]);
+  //   }
+  
+  // HTMLCanvasElement.prototype.renderImage = function(blob){
+    
+  //   var ctx = this.getContext('2d');
+  //   var img = new Image();
+  
+  //   img.onload = function(){
+  //     ctx.drawImage(img, 0, 0)
+  //   }
+  
+  //   img.src = URL.createObjectURL(blob);
+  // };
+  
+  // input.addEventListener('change', picToBlob, false);
+
+
