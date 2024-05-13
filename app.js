19fd02dc490b89413c7a59d801f0be0d21599851
Georg <gwbootcamp24.google.com>
2024-05-13 02:58:14 +0200

    did some resizing in Render

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 // app.use(express.json());
-app.use(express.json({ limit: '20000kb' }));
+app.use(express.json({ limit: '40000kb' }));
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('.'));
 
